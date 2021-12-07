b4f09e669ac4fd0edbe885210f793c77017b2d89
Hugh Nguyen <hhnguyen255@gmail.com>
2021-12-07 17:08:16 +1100

    update deployment script

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
-// import { ServeStaticModule } from "@nestjs/serve-static";
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { resolve } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: resolve(__dirname, '..', 'client', 'build'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
